26288116feffc4f63728f46ecdc7a9ffd3075cba
FreeCalradiaNow <111363541+FreeCalradiaNow@users.noreply.github.com>
2022-08-31 08:26:14 +0200

    Initial creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palin_Ident
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
