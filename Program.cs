e413067a21bd32b6a9dc9c8d29000533635e9842
m.witzik <m.witzik@B-WRK-0224>
2022-08-31 11:27:58 +0200

    Frame 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Palin_Ident
 {
@@ -6,7 +7,35 @@ namespace Palin_Ident
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n\tplaceholder_welcome\n\n\tplaceholder_inputRequest");
+
+            string inputWord = Console.ReadLine();
+
+            char[] inputChar = inputWord.ToCharArray();
+
+            int inputCount = inputChar.Length;
+
+            bool evenCount = (inputCount % 2 == 0);
+
+            Char[] firstHalf = (inputChar.Length / 2);
+
+            if (evenCount == true)
+            {
+               
+            }
+            else
+            {
+                //Console.WriteLine("ungerade");
+            }
+
+           
+
+            Console.WriteLine(inputChar);
+          
+
+
+
+
         }
     }
 }
